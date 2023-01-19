166f1459f29e363c354adfd69cd6c33d5cb744a4
Jesus <jesusalcocer5005@gmail.com>
2023-01-19 08:45:18 +0100

    Dia2

---
+++ Dia2/variables.py
@@ -0,0 +1,54 @@
+first_name = 'Pablo'
+last_name = 'Huertas'
+full_name = 'Pablo Huertas'
+country = 'España'
+city = 'Jerez'
+age = '16'
+year = '2006'
+is_married = False
+is_true = True
+is_light_on = True
+first_name , last_name , full_name , country , age , is_married = 'Pablo' , 'Huertas' , 'Pablo Huertas' , 'country', '16' , False
+print(type('Pablo'))
+print(type(10))
+print(type(3.14))
+print(type(1 + 1j))
+print(type(True))
+print(type([1, 2, 3, 4]))
+print(type({'name':'Pablo':'17':250, 'is_married':250}))
+print(type((1,2)))
+print(type(zip([1,2],[3,4])))
+
+print(type(Pablo))
+print(type(Huertas))
+number_one = 4
+number_two = 8
+suma = number_one + number_two
+resta = number_one - number_two
+number_one / number_two
+number_one % number_two = 4
+number_one ** number_two = 65536
+number_one // number_two = 0
+
+ π = 3.14
+ radio=int(input("30"))
+ area_of_the_circle=π*radio*radio
+ print(el area es:"2826")
+ circum_of_the_circle=2*π*radio
+ print(la circunferencia= "188,4")
+ radio=int(input("30"))
+ area_of_the_circle=π*radio*radio
+ print(el area es:"2826")
+
+ first_name = input("What is your name?")
+ last_name = input("What is your last name?")
+ age = input("How old are yuo?")
+ country = input("What is your country")
+ print(first_name=Pablo)
+ print(last_name=Huertas)
+ print(age=16)
+ print(country=Spain)
+
+ help ("keywords")
+ False               class             from                or
+
