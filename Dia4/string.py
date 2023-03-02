4a282f5eeb830e1d0dfe79a39cdccaaeeea3080d
Jesus <jesusalcocer5005@gmail.com>
2023-03-02 09:06:42 +0100

    Dia5

--- Dia4/string.py
+++ Dia4/string.py
@@ -13,5 +13,22 @@ print(company.upper)
 print(company.lower)
 print(company)
 
-remove_coding = company[7:15]
+remove_coding = company[4:14]
 print(remove_coding)
+
+print(company.index('Coding'))
+print(company.find('Coding'))
+print(company.replace('Coding', 'Python'))
+sentense = 'Python for Everyone'
+print(company.replace('Everyone', 'All'))
+print(company.split())
+first_letter = company[5]
+print(first_letter)
+last_letter = company[-5]
+print(last_letter)
+tenth_index = company[10]
+print(tenth_index)
+print(company.index('C'))
+print(company.index('F'))
+last_1 = '1'
+print(company.find(last_1, 13))
---
+++ Dia5/list.py
@@ -0,0 +1,21 @@
+emptyList = []
+household_items = ['coffee', 'cereals', 'pots', 'air-conditions', 'chairs']
+length = len(household_items)
+print(length)
+first_item = household_items[0]
+second_item = household_items[1]
+third_item = household_items[2]
+print(first_item)
+print(second_item)
+print(third_item)
+mixed_data_types = ['Pablo', 34, 4.5, False, {'Huertas': '10H'}]
+print(mixed_data_types)
+it_companies = ['facebook', 'google', 'microsoft', 'apple', 'INM', 'oracle', 'amazon']
+print(it_companies)
+print(len(it_companies))
+first_it_companies = it_companies[0]
+second_it_companies = it_companies[4]
+third_it_companies = it_companies[-2]
+print(first_it_companies)
+print(second_it_companies)
+print(third_it_companies)
