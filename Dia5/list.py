bddcafa8782fd96be981e5d3d45ab7921ec0970a
Jesus <jesusalcocer5005@gmail.com>
2023-03-03 10:42:07 +0100

    Dia5

--- Dia5/list.py
+++ Dia5/list.py
@@ -19,3 +19,26 @@ third_it_companies = it_companies[-2]
 print(first_it_companies)
 print(second_it_companies)
 print(third_it_companies)
+it_companies[1] = 'melón'
+print(it_companies)
+it_companies.append('sandía')
+print(it_companies)
+it_companies.insert(3, 'pera')
+print(it_companies)
+mayúscula = it_companies[2].upper()
+print(mayúscula)
+joiningn = '#,'.join(it_companies)
+print(joiningn)
+does_it_exist = 'manzana' in it_companies
+print(does_it_exist)
+it_companies.sort()
+print(it_companies)
+it_companies.sort(reverse=True)
+print(it_companies)
+first_three_items = it_companies[0:3]
+print(first_three_items)
+last_three_items = it_companies[5:]
+print(last_three_items)
+middle_item = it_companies[4:3]
+print(middle_item)
+
